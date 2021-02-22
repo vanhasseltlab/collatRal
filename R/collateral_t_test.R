c5a0d3d2947980e4149deb5bf5055fbb92ca83a7
laurazwep <l.b.zwep@lacdr.leidenuniv.nl>
2021-02-22 10:59:32 +0100

    add function collateral_t_test

---
+++ R/collateral_t_test.R
@@ -0,0 +1,78 @@
+collateral_t_test <- function(A, B, CE_type = "both", crit_type = "median",
+                              criterium = NULL) {
+  # Performs T-test on vector A, which is split by a dichotomization of vector B
+  #
+  # Args:
+  #   A: MIC values antibiotic A, numeric vector
+  #   B: MIC values antibiotic B, numeric vecter, same length as A
+  #   CE_type: type of collateral effect to be evaluated, string: "both", "CR" or "CS"
+  #   crit_type: type of dichotomization criterion, string: "quant", "median" or "log2_MIC"
+  #   criterium: value of dichotomization criterium, single numeric, overwritten if crit_type = "median"
+  #
+  # Returns:
+  #   List with class "htest" including statistic, parameter, p.value, etc.
+
+  #Error handling
+  if (length(A) != length(B)) {
+    stop("Arguments A and B have different lengths: ", length(A), " and ", length(B), ".")
+  }
+
+  #Remove NA's
+  ind_na <- is.na(A) | is.na(B)
+  A <- log2(A[!ind_na])
+  B <- log2(B[!ind_na])
+
+  #Translate CE_type to t-test direction
+  direction <- c("two.sided", "less", "greater")[c("both", "CS", "CR") == CE_type]
+
+  #Calculate dichotomization criterium tau based on criterium type
+  if (crit_type == "quant") {
+    tau <- quantile(B, criterium)
+
+  } else if (crit_type == "median") {
+
+    tau <- stats::quantile(B, 0.5)
+    B_values <- sort(unique(B))
+    #Adjust tau to create most equal split
+    if (sum(B > tau) < sum(B < tau)) {
+      tau <- mean(c(B_values[which(B_values == tau) - 1], tau))
+    } else if (sum(B > tau) > sum(B < tau)) {
+      tau <- mean(c(B_values[which(B_values == tau) + 1], tau))
+    }
+
+  } else {
+    tau <- criterium
+    if (!crit_type %in% c("quant", "median", "log2_MIC")) {
+      warning("crit_type is not specified (correctly), defaults to \"log2_MIC\"")
+    }
+  }
+
+  A_Blow <- A[B < tau]
+  A_Bhigh <- A[B >= tau]
+
+  #Error handling
+  if (length(A_Blow) < 2 | length(A_Bhigh) < 2) {
+    warning("Not enough observations for test! Returning list with limited information. Try different dichotomization criterium for test.")
+    return(list(A = A, B = B, tau = tau, data = list(`A|B = r` = A_Bhigh,
+                                                     `A|B != r` = A_Blow)))
+  }
+
+  #Perform t.test
+  A_t_test <- stats::t.test(A_Bhigh, A_Blow, var.equal = TRUE, alternative = direction)
+
+  #Adjust names of groups
+  names(A_t_test$estimate) <- c("mean of A|B = high", "mean of A|B = low")
+  A_t_test$data.name <- "A|B = high and A|B = low"
+
+  #Add data to output
+  A_t_test$data <- list(`A|B = high` = A_Bhigh, `A|B = low` = A_Blow)
+
+  #Add tau to object
+  A_t_test$tau <- tau
+
+  #Add log2(fold change) tot output
+  A_t_test$log2_FC <- A_t_test$estimate[1] - A_t_test$estimate[2]
+  names(A_t_test$log2_FC) <- "log2_FC"
+
+  return(A_t_test)
+}
